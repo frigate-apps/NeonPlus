0da8533cdc8675a9be223b114bc6cb51ab0449ae
BeanFlame (豆焰) <beanflame@qq.com>
2024-02-14 04:09:16 +0800

    Create File.ts

---
+++ z_code/File.ts
@@ -0,0 +1,24 @@
+import stdio;
+
+public type File {
+
+    # File 1.0 
+    public type File() {
+        #代码
+    }
+
+    public type ~File() {
+        # 删除类型, 清除内存
+    }
+  
+    public func type main (){
+ 
+    }
+    
+    public type func oran () {
+    
+    }
+    
+  
+
+}
