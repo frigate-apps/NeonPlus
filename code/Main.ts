9a55795782d2fef5cfb459cc8168d2ed57359ea5
BeanFlame (豆焰) <beanflame@qq.com>
2024-02-14 04:03:47 +0800

    Update and rename Main.np to Main.ts

---
+++ code/Main.ts
@@ -0,0 +1,16 @@
+import neon;
+
+public type Main {
+    
+    public type Main {
+        
+        public type func void main (String[] args) {
+            System.out.println("Hello World!");
+        }
+        
+        let addNum(let a, let b) {
+            return a + b ;
+        }
+        
+    }
+}
