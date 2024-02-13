1e38b116afc077341ca16c47d8646c44c459f4fb
BeanFlame (豆焰) <beanflame@qq.com>
2024-02-14 04:04:46 +0800

    Update Main.ts

--- code/Main.ts
+++ code/Main.ts
@@ -1,9 +1,12 @@
 import neon;
-
+import neon.type;
+import neon.let;
+import neon.func;
+import neon.string;
 
 public type Main {
     
-    public type func void main (String[] args) {
+    public type func void main (string[] args) {
         System.out.println("Hello World!");
     }
     
