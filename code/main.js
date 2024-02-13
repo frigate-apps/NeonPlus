92ff1149b2820696d6d8c8d4a4c1618faacfa6d6
BeanFlame (豆焰) <beanflame@qq.com>
2024-02-14 03:49:42 +0800

    Update and rename main.np to main.js

---
+++ code/main.js
@@ -0,0 +1,11 @@
+import stdio;
+func type main () {
+    printf("Hello World!");
+    // console.writeln('Hello World!');
+    let int e = 1;
+    let char[] makr = "Haex";
+    printf("Hello World!");
+    console.writeln("Hello World!");
+}
+
+
