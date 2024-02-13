f24a4f134d5cb5bffbdf4518c080d9e75145ad4a
BeanFlame (豆焰) <beanflame@qq.com>
2024-02-14 03:57:55 +0800

    Update hello.ts

--- z_code/hello.ts
+++ z_code/hello.ts
@@ -1,27 +1,31 @@
 import stdio;
 
-func type main () {
-    let int e = 1;
-    let char[] makr = "Haex";
-    printf("Hello World!");
-    console.writeln("Hello World!");
-}
+type Jrmo {
+    
+    func type main () {
+        let int e = 1;
+        let char[] makr = "Haex";
+        printf("Hello World!");
+        console.writeln("Hello World!");
+    }
+    
+    type oran () {
+        ito.sge.itbwr();
+        time.mker();
+        los.nsJr();
+        mior.Eqpon();
+    }
+    
+    int oere (int a, int b) {
+        return a + b;
+    }
+    
+    let addNum(let a, let b) {
+        return a + b;
+    }
+    
+    var addNum(var a, var b) {
+        return a + b;
+    }
 
-type oran () {
-    ito.sge.itbwr();
-    time.mker();
-    los.nsJr();
-    mior.Eqpon();
-}
-
-int oere (int a, int b) {
-    return a + b;
-}
-
-let addNum(let a, let b) {
-    return a + b;
-}
-
-var addNum(var a, var b) {
-    return a + b;
 }
